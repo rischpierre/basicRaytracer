9423bbef42184ed846009e022a0e8950396dfe9b
pierre <rischpierre@gmail.com>
2021-10-23 15:06:13 -0400

    add tests back and create non intersecting triangle

--- tests/test_raytracer.cpp
+++ tests/test_raytracer.cpp
@@ -9,50 +9,43 @@ TEST(raytracer, test_ray_inside_triangle){
                .direction={0, 1, 0}};
 
     Scene s;
-    parseObjFile(&s, "../examples/triangle.obj");
+    parseObjFile(&s, "../../examples/triangle.obj");
     ASSERT_TRUE(isRayIntersectsTriangle(&ray, s.object.faces));
 }
-//
-//TEST(raytracer, test_ray_outside_triangle){
-//    Ray ray = {.origin={0, 0, 0},
-//               .direction={1, 0, 0}};
-//
-//    // defining a triangle that is not in the sight of the ray
-//    Face f1 = {.v0={2, 2, 2},
-//               .v1={2, -1, 1},
-//               .v2={2, -1, 3},
-//               .normal={-1, 0, 0}};
-//
-//    ASSERT_FALSE(isRayIntersectsTriangle(&ray, &f1));
-//}
-//
-//TEST(raytracer, test_ray_inside_tilted_triangle){
-//    Ray ray = {.origin={0, 0, 0},
-//            .direction={1, 0, 0}};
-//
-//    // defining a triangle that is not in the sight of the ray
-//    Face f1 = {.v0={1, 2, 2},
-//            .v1={2, -1, 1},
-//            .v2={1, -1, 3},
-//            .normal={-1, 0, 0}};
-//
-//    ASSERT_FALSE(isRayIntersectsTriangle(&ray, &f1));
-//}
-//
-//TEST(computeColor, test_computeColor){
-//    DirLight light = {.direction={1, 0, 0}};
-//    float faceN[3] = {-1, 0, 0};
-//
-//    ASSERT_EQ(computeColor(faceN, &light), 1.f);
-//
-//    faceN[0] = -0.5f;
-//    faceN[1] = -0.5f;
-//    faceN[2] = 0;
-//    ASSERT_FLOAT_EQ(computeColor(faceN, &light), 0.5f);
-//
-//    faceN[0] = 1.f;
-//    faceN[1] = 0;
-//    faceN[2] = 0;
-//    ASSERT_FLOAT_EQ(computeColor(faceN, &light), 0);
-//}
-//
+
+TEST(raytracer, test_ray_outside_triangle){
+    Ray ray = {.origin={0, 0, 0},
+            .direction={0, 1, 0}};
+
+    Scene s;
+    parseObjFile(&s, "../../examples/notIntersectingYTriangle.obj");
+    ASSERT_FALSE(isRayIntersectsTriangle(&ray, s.object.faces));
+}
+
+TEST(raytracer, test_ray_inside_tilted_triangle){
+    Ray ray = {.origin={0, 0, 0},
+            .direction={0, 1, 0}};
+
+    // todo this test is failing
+    Scene s;
+    parseObjFile(&s, "../../examples/tiltedTriangle.obj");
+    ASSERT_FALSE(isRayIntersectsTriangle(&ray, s.object.faces));
+}
+
+TEST(computeColor, test_computeColor){
+    DirLight light = {.direction={1, 0, 0}};
+    float faceN[3] = {-1, 0, 0};
+
+    ASSERT_EQ(computeColor(faceN, &light), 1.f);
+
+    faceN[0] = -0.5f;
+    faceN[1] = -0.5f;
+    faceN[2] = 0;
+    ASSERT_FLOAT_EQ(computeColor(faceN, &light), 0.5f);
+
+    faceN[0] = 1.f;
+    faceN[1] = 0;
+    faceN[2] = 0;
+    ASSERT_FLOAT_EQ(computeColor(faceN, &light), 0);
+}
+
